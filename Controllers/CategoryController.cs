da739143e80d5c829856468df2eca761b7b27d3a
Aravind55597 <67952928+Aravind55597@users.noreply.github.com>
2021-05-13 22:48:09 +0800

    Add project files.

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Controllers
+{
+    public class CategoryController: Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+
+
+        //GET INDEX
+        public IActionResult Index()
+        {
+            //Retrieves all the categories from the database 
+            //Assigning to IEnumerable make it readonly
+            IEnumerable<Category> objList = _db.Category; //select * from category 
+            return View(objList); //we pass the list to the view 
+
+        }
+
+
+        //GET_CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        //POST_CREATE
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            //add the category object to the database
+            _db.Category.Add(obj);
+            //save the changes 
+            _db.SaveChanges();
+            return View();
+        }
+
+
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Models
+{
+    public class Category
+    {
+
+        [Key] //Tells that the property will be a primary key 
+
+        //Id column autoincremenets in the database 
+        //if the name of the property is Id or CategoryId , it iwill be auto recognised as a primary entity 
+        public int Id { get; set; }
+
+        [DisplayName("Category Name")]
+        public string CategoryName { get; set; }
+
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+
+
+
+
+    }
+}
